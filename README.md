7e3751a50f161e8933719a4c9ceb4508c62ff6d4
AndreaAntonello <a.an@automata.tech>
2020-05-18 18:51:57 +0100

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# quaternionUtilitiesPython
+# Quaternion Utilities
+
+This repo contains methods to allow transformation from quaternion 
+to other common 3D representation method.
+
+In particular, the tools contained are:
+    
+* **wrap_to_pi**: this method to wrap an angle (in rad) to the range [-pi; pi]
+* **quat_conj**: this method returns the conjugate (inverse) of a quaternion
+* **quat_multiply**: this method returns the product of a quaternion multiplication
+* **vect_normalize**: this method normalizes a 1x3 vector
+* **quat_normalize**: this method normalizes a quaternion
+* **quat_norm**: this method returns the norm of a quaternion
+* **quat_to_rotm**: this method returns the Direction Cosine Matrix (DCM) by applying the Euler-Rodrigues parameterization, given an input quaternion
+* **rotm_to_quat**: this method returns the quaternion by applying the Euler-Rodrigues parameterization, given an input Direction Cosine Matrix (DCM)
+* **axisangle_to_quat**: this method returns the quaternion from the input axis and angle representation
+* **quat_to_axisangle**: this method returns the axis and angle representation from the input quaternion
+* **rpy_to_quat**: this method returns the quaternion from roll, pitch, yaw angle (ZYX Euler sequence)
+* **quat_to_rpy**: this method returns the roll, pitch, yaw angle (ZYX Euler sequence) from the quaternion
+
--- test_quaternion.py
+++ test_quaternion.py
@@ -1,9 +1,56 @@
-from tools.quatAlgebra import *
+from tools.quat_tools import *
 
 
-q = [2516, 1, 3, -20]
-print(len(q))
+# Quaternion conjugate
+q = [0.7071, 0.7071, 0.0, 0.0]
+q_conj = quat_conj(q)
+print('Quaternion conjugate is', q_conj)
 
-rotm = np.zeros((3, 3))
 
-print(rotm_to_quat(rotm))
+# Quaternion norm/normalize
+q = [7071, 7071, 0.0, 0.0]
+q_norm = quat_norm(q)
+print('Original norm is ', q_norm)
+
+q_norm = quat_norm(quat_normalize(q))
+print('Normalized norm is ', q_norm)
+
+
+# Quaternion multiply
+q0 = [0, 1, 0, 0]
+q1 = [0, 0, 1, 0]
+q_mult = quat_multiply(q0, q1)
+print('Quaternion multiplication result is ', q_mult)
+
+
+# Quaternion <-> rotation matrix
+rotm = np.array([[1, 0, 0], [0, -1, 0], [0, 0, -1]])
+q = rotm_to_quat(rotm)
+print('Quaternion is', q)
+
+rotm = quat_to_rotm(q)
+print('Rotation matrix is', rotm)
+
+
+# Quaternion <-> axis angle
+angle = 0.12345
+axis = [1, 2, 3]
+axis_norm = vect_normalize(axis)
+print('Axis normalized is', axis_norm)
+
+q = axisangle_to_quat(axis_norm, angle)
+print('Quaternion is', q)
+
+axis, angle = quat_to_axisangle(q)
+print('Axis is', axis, 'Angle is', angle)
+
+
+# Quaternion <-> roll, pitch, yaw (Euler 'ZXY')
+yaw = 0.1
+pitch = 0.2
+roll = 0.3
+q = rpy_to_quat(roll, pitch, yaw)
+print('Quaternion is', q)
+
+roll, pitch, yaw = quat_to_rpy(q)
+print('Roll =', roll, 'pitch=', pitch, 'yaw=', yaw)
